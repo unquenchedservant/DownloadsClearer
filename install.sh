901370fc474cc42792063cf6d9a78276a4e2c178
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-05-01 10:36:41 -0400

    Updated comments

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 if [ "$EUID" -ne 0 ]
-    then echo "Please run as root" # This may not be necessary, but I was testing something
+    then echo "Please run as root"
 fi
 
 
 # General install psuedocode
 # use wget to install DownloadClearer to /usr/bin/ (possible aliases: dc)
 # first I need to get DownloadClearer on github 
+# then chmod +x DownloadClearer
+# this actually probably needs sudo priviledges. (it does)
+
