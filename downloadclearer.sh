cc21590b352495007b7f9b03ef63188f9d27c441
Jonathan Thorne <jonathanthorne13@gmail.com>
2022-05-01 11:07:52 -0400

    cleared unneeded flags

--- downloadclearer.sh
+++ downloadclearer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts y:m:w:d:h:M:s flag
+while getopts y:m:d:h:M flag
 do
     case "${flag}" in
         y) 
@@ -23,20 +23,6 @@ do
                 fi 
             else
                 months=0;
-            fi
-            ;;
-        w)
-            if [[ ${OPTARG} ]]
-            then
-                if [[ ${OPTARG} -gt 3 ]] #going with the standard of 4 weeks to a month, i know it isn't pretty
-                then
-                    echo "Weeks must be less than 4";
-                    exit 0;
-                else
-                    weeks=${OPTARG};
-                fi
-            else
-                weeks = 0;
             fi
             ;;
         d)
@@ -81,26 +67,10 @@ do
                 minutes=0;
             fi
             ;;
-        s)
-            if [[ ${OPTARG} ]]
-            then
-                if [[ ${OPTARG} -gt 59 ]]
-                then
-                    echo "Seconds must be less than 60";
-                    exit 0;
-                else
-                    seconds=${OPTARG};
-                fi
-            else
-                seconds=0;
-            fi
-            ;;
     esac
 done
 echo "Years: $years";
 echo "Months: $months";
-echo "Weeks: $weeks";
 echo "Days: $days";
 echo "Hours: $hours";
 echo "Minutes: $minutes";
-echo "Seconds: $seconds";
